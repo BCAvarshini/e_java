eb5f02dedae0bad09c1a7687a341d24a6a13541d
BCAvarshini <varshinijakkali@gmail.com>
2025-04-08 12:05:24 +0530

    saved

--- adding.java
+++ adding.java
@@ -5,6 +5,7 @@ public class adding {
         int a = 5, b = 6;
         int sum = a + b;
         System.out.println("Addition of 2 nor " + sum);
+        System.out.println("I Have A Car");
     }
 
 }
