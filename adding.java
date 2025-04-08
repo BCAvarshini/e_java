d2f436921a847f44f89db103a0d6b1735f5a2085
BCAvarshini <varshinijakkali@gmail.com>
2025-04-08 11:55:14 +0530

    saved

---
+++ adding.java
@@ -0,0 +1,10 @@
+
+public class adding {
+
+    public static void main(String[] args) {
+        int a = 5, b = 6;
+        int sum = a + b;
+        System.out.println("Addition of 2 nor " + sum);
+    }
+
+}
